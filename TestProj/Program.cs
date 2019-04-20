a9b32e3c0f49e4562722e2bbb864fa0f7cb1853f
fateev <fateev@104-01>
2019-04-20 12:24:51 +0300

    Вывод имени Арнольда

--- TestProj/Program.cs
+++ TestProj/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TestProj
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Arnold!");
+
+            Console.Read();
         }
     }
 }
